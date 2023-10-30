8575c2410973c3373d9d92e72efc5dc16fb8a94f
RORY MCCONNELL <skunkbentley@hotmail.com>
2023-10-30 17:49:38 +0000

    basic page test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  async function getLocation(event) {
+    event.preventDefault();
+
+    //API URL REQUEST
+    const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
+
+    //make a GET REQUEST
+    const response = await axios.get(API);
+
+    setLocation(res.data[0]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>!!!!!!</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} placeholder="Enter Location" />
+        <button>Get Location</button>
+      </form>
+
+      <h2>{location.display_name}</h2>
+      {/* a form with an input for the user to search */}
+      {/* information about the location saved in state */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
